264b0dc0a24762cd62096a95207cbe5d1195fb95
ysk <22higa_yuusuke@axiz.co.jp>
2022-06-20 06:48:03 +0900

    Service

---
+++ src/main/java/com/example/demo/service/DownloadService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+
+import java.util.List;
+
+import com.example.demo.entity.Image;
+
+public interface DownloadService {
+	// ユーザー毎にダウンロードを管理
+	public List<Image> findByUserId(Integer userId);
+
+	// ダウンロード数を数える
+	public int countDownload(Integer imageId);
+
+	// ダウンロードされたときユーザーと画像をダウンロードしたことを処理
+	public int insert(Integer userId, Integer imageId);
+}
