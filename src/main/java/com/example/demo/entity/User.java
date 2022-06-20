56d95a9ccf422fb343411bfdc62dbb29931d0a61
yujiishadou <22ishado_yuuji@axiz.co.jp>
2022-06-20 10:26:21 +0900

    entity、daoの追加 pgdaoは空

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,68 @@
+package com.example.demo.entity;
+
+public class User {
+	private int id;
+	private String accountId;
+	private String password;
+	private String name;
+	private int role;
+	private String iconPath;
+	private String mail;
+	private String introduction;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getRole() {
+		return role;
+	}
+	public void setRole(int role) {
+		this.role = role;
+	}
+	
+	public String getIconPath() {
+		return iconPath;
+	}
+	public void setIconPath(String iconPath) {
+		this.iconPath = iconPath;
+	}
+	
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	
+	public String getIntroduction() {
+		return introduction;
+	}
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+}
