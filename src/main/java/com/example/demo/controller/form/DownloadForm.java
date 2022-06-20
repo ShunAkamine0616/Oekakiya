957a7c6f65c68a7b4c34b9b3e21c34d8ae552d7f
ysk <22higa_yuusuke@axiz.co.jp>
2022-06-20 12:50:21 +0900

    ダウンロードフォーム

---
+++ src/main/java/com/example/demo/controller/form/DownloadForm.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller.form;
+
+public class DownloadForm {
+	private Integer id;
+	private Integer userId;
+	private Integer imageId;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getImageId() {
+		return imageId;
+	}
+	public void setImageId(Integer imageId) {
+		this.imageId = imageId;
+	}
+
+
+
+}
