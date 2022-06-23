9af3e2cae398e06394aa3419301a094daea33800
ysk <22higa_yuusuke@axiz.co.jp>
2022-06-23 17:27:52 +0900

    カテゴリDAO追加

--- src/main/java/com/example/demo/dao/CategoryDao.java
+++ src/main/java/com/example/demo/dao/CategoryDao.java
@@ -6,6 +6,7 @@ import com.example.demo.entity.Category;
 
 public interface CategoryDao {
 	public List<Category> findAll();
+	public Category findByCategoryId(Integer id);
 	public int insert(String categoryName);
 	public Integer update(Integer id , String categoryName);
 	public Integer delete(Integer categoryId);
