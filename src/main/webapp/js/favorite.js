5a108e54838b631ee7f077171dba2065c5695fda
ysk <22higa_yuusuke@axiz.co.jp>
2022-06-23 14:54:42 +0900

    ページを読み込んだ時にしたい処理を追加

--- src/main/webapp/js/favorite.js
+++ src/main/webapp/js/favorite.js
@@ -2,6 +2,22 @@
 var nonImg = document.getElementById("nonHurt");
 var yesImg = document.getElementById("yesHurt");
 
+// ページ読み込み時に実行したい処理
+document.addEventListener('DOMContentLoaded', function(){
+fetch("checkFavorite")
+.then(response => response.text())
+.then(data => {
+	console.log(data);
+	if(data == 1) {
+		nonImg.className = "hidden";
+		yesImg.className = null;
+	} else {
+		nonImg.className = null;
+		yesImg.className = "hidden";
+	}
+	
+})});
+
 nonImg.addEventListener('click',event =>{
 	fetch("FavoriteCount")
 .then(response => response.text())
