e2262c332537b3f430de8a779c6183633c0104b2
ueharajuki <22uehara_juki@axiz.co.jp>
2022-06-27 10:15:30 +0900

    インサートjsp削除 バリテーション追加

--- src/main/java/com/example/demo/controller/form/EditForm.java
+++ src/main/java/com/example/demo/controller/form/EditForm.java
@@ -1,20 +1,26 @@
 package com.example.demo.controller.form;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 
 public class EditForm{
 	
+	@Max(value=100, message="タイトルは100文字以内です。  ")
 	@NotBlank(message="画像のタイトルは必須です。  ")
 	private String title;
 	
 	private Integer categoryId;
+	
+	@Max(value=2000, message="コメントは2000文字以内です。  ")
+	private String comment;
+	
 	public Integer getCategoryId() {
 		return categoryId;
 	}
 	public void setCategoryId(Integer categoryId) {
 		this.categoryId = categoryId;
 	}
-	private String comment;
+
 	public String getTitle() {
 		return title;
 	}
